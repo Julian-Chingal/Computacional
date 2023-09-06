0243e619d1616da5c7a305cbe0f0e7b2c684cf6f
Julian-Chingal <mauriciochingal16@gmail.com>
2023-09-06 00:57:50 -0500

    Points Red, Blue

--- Entrega_1/visioncomputacional.py
+++ Entrega_1/visioncomputacional.py
@@ -7,20 +7,49 @@ cutImage = None
 wewidth_cut = None
 High_cut = None
 
-#CIRCULOS DE COLOR AZUL Y ROJO PARA EL PUNTO
-
 #functions ------------------------------------------------------------------------------
 def getPoints (event,x,y,flags, param):
   if event == cv.EVENT_LBUTTONDOWN:
     srcPoints.append((x,y))
     print("Punto agregado: ", x, y)
 
-def DrawContours(blur):
-  #Canny detection
-    threshold1  = cv.getTrackbarPos("Threshold1", "Parameters")
-    threshold2  = cv.getTrackbarPos("Threshold2", "Parameters")
-    canny = cv.Canny(blur, threshold1 ,threshold2)
+def refPoints():
+  global cutImage
 
+  #saturar la imagen 
+  hsv_frame = cv.cvtColor(cutImage, cv.COLOR_BGR2HSV)
+
+  #Definir rangos de color
+  lower_red = np.array([0, 100, 100])
+  upper_red = np.array([10, 255, 255])
+
+  lower_blue = np.array([100, 100, 100])
+  upper_blue = np.array([130, 255, 255])
+  
+  #Filtra los píxeles de color rojo y azul
+  mask_red = cv.inRange(hsv_frame, lower_red, upper_red)
+  mask_blue = cv.inRange(hsv_frame, lower_blue, upper_blue)
+
+  # Encuentra círculos en las máscaras de color rojo y azul
+  circles_red = cv.HoughCircles(mask_red, cv.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=0, maxRadius=0)
+  circles_blue = cv.HoughCircles(mask_blue, cv.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=0, maxRadius=0)
+
+  if circles_red is not None:
+    # Dibuja los círculos rojos encontrados
+    circles_red = np.uint16(np.around(circles_red))
+    for circle in circles_red[0, :]:
+      center = (circle[0], circle[1])
+      cv.circle(cutImage, center, circle[2], (0, 0, 255), 2)
+
+  if circles_blue is not None:
+    # Dibuja los círculos azules encontrados
+    circles_blue = np.uint16(np.around(circles_blue))
+    for circle in circles_blue[0, :]:
+      center = (circle[0], circle[1])
+      cv.circle(cutImage, center, circle[2], (255, 0, 0), 2)
+
+def DrawContours(canny):
+    global cutImage
   #Contours
     contours, _  = cv.findContours(canny,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
@@ -44,16 +73,9 @@ def Preprocess(frame):
   dstPoints = np.array([[0, 0], [frame.shape[1], 0], [frame.shape[1], frame.shape[0]], [0, frame.shape[0]]], dtype=np.float32)
   #perspective transform
   homography, _ = cv.findHomography(np.float32(srcPoints), dstPoints)
-
   img_undistorted = cv.undistort(frame, np.eye(3), np.zeros(5)) # Corregir distorsión no lineal
-
   cutImage = cv.warpPerspective(img_undistorted, homography, (frame.shape[1], frame.shape[0]))
 
-  wewidth_cut = cutImage.shape[1]
-  High_cut = cutImage.shape[0]
-
-  print("ancho: " , wewidth_cut, " Alto: ", High_cut)
-   
   #grayscale
   gray = cv.cvtColor(cutImage,cv.COLOR_BGR2GRAY)  
 
@@ -67,6 +89,7 @@ def Preprocess(frame):
 
 def empty(a):
   pass
+#---------------------------------------------------------------------------------------------------------------------
 
 #video capture
 cap = cv.VideoCapture(2)
@@ -80,7 +103,7 @@ cv.createTrackbar("Threshold1", "Parameters", 100, 255, empty)
 cv.createTrackbar("Threshold2", "Parameters", 150, 255, empty)
 cv.createTrackbar("Area", "Parameters",5000,40000, empty)
 
-#stage capture -------------------------------------------------------------------------------------------
+#start  -------------------------------------------------------------------------------------------
 while (cap.isOpened()):
   ret, frame = cap.read()
   frame = cv.flip(frame, 1)     #eliminar efecto espejo
@@ -92,8 +115,14 @@ while (cap.isOpened()):
     #Preproces
     blur  = Preprocess(frame)
 
+    #Canny detection
+    threshold1  = cv.getTrackbarPos("Threshold1", "Parameters")
+    threshold2  = cv.getTrackbarPos("Threshold2", "Parameters")
+    canny = cv.Canny(blur, threshold1 ,threshold2)
+    
     #contours
-    canny = DrawContours(blur)
+    refPoints()
+    DrawContours(canny)
 
     #Show
     cv.imshow('Cut', cutImage)
@@ -103,8 +132,6 @@ while (cap.isOpened()):
     for corner in srcPoints: #mostrar los puntos
       cv.circle(frame, corner, 2, (0,0,255),-1)
     cv.imshow('Original', frame)
-  
-  print()
   #exit
   if cv.waitKey(1) & 0xFF == ord('q'):
     break
